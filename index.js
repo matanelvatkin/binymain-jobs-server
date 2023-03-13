9cd6e021d9cb1c3e32ff137408c3c18d0045b4eb
kobikru <75983501+kobikru@users.noreply.github.com>
2023-03-14 01:27:17 +0200

    multer+static

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ const PORT = process.env.PORT || 2000;
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
+app.use("/static",express.static("./upload"))
 
 app.use("/api", mainRouter);
 
+
+// app.get("/k",(req,res)=>{
+// res.send("hello world")
+// });
 app.listen(PORT, () => {
   console.log("Server is running : listening to port " + PORT);
 });
