866166181af1b70d8975ccc2337a9e774d26d865
yisrael <Yisrael94@gmail.com>
2023-03-13 17:46:37 +0200

    deleted the callback in the db

--- DL/db.js
+++ DL/db.js
@@ -6,13 +6,9 @@ async function connect() {
   try {
     mongoose.connect(
       MONGO_URL,
-      { useNewUrlParser: true, useUnifiedTopology: true },
-      (err) => {
-        if (err) throw "Error DB : " + err;
-
-        console.log(`Connection Success`);
-      }
-    );
+      { useNewUrlParser: true, useUnifiedTopology: true }
+      
+    )
   } catch (error) {
     console.log(error);
     throw error;
