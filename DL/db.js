55a4893e1c9fa96704cf7a6e335901ae96006351
matanel vatkin <matanelvatkin@h-MacBook-Pro-sl-matanel.local>
2023-03-15 13:08:54 +0200

    fixing diplyoming 1.0

--- DL/db.js
+++ DL/db.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 const MONGO_URL = process.env.MONGO_URL;
 
 async function connect() {
   try {
     mongoose.connect(
       MONGO_URL,
-      { useNewUrlParser: true, useUnifiedTopology: true }
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      (err) => {
+        if (err) throw "Error DB : " + err;
+
+        console.log(`Connection Success`);
+      }
     );
   } catch (error) {
     console.log(error);
